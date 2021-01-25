d2ac6fdc7a170009845c2009d034073612466d51
Andtii <andy@ekdahls.net>
2021-01-25 10:56:34 +0100

    Error statuscode if api access error

--- src/breaking/Omnia.Breaking.Tests/Program.cs
+++ src/breaking/Omnia.Breaking.Tests/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Omnia.Breaking.Tests
@@ -10,14 +11,18 @@ namespace Omnia.Breaking.Tests
         static async Task Main(string[] args)
         {
 
+            //var local = "https://localhost:44365/api/";
+            var cloud = "https://5d18a2ba13d247fcb77fc24024c5364b-omniadevops.omniatestcloud.net/api/";
+
+            Console.WriteLine("Sleeping 15 seconds before running");
+            Thread.Sleep(15000);
+
+            Console.WriteLine($"Trying to get values from {cloud}values");
 
             HttpClientHandler handler = new HttpClientHandler()
             {
                 AutomaticDecompression = DecompressionMethods.Brotli | DecompressionMethods.GZip | DecompressionMethods.Deflate
             };
-
-            //var local = "https://localhost:44365/api/";
-            var cloud = "https://5d18a2ba13d247fcb77fc24024c5364b-omniadevops.omniatestcloud.net/api/";
 
             var nugetClient = new HttpClient(handler);
             nugetClient.DefaultRequestHeaders.Add("Accept", "application/json");
@@ -26,7 +31,11 @@ namespace Omnia.Breaking.Tests
 
             if (resp.StatusCode != HttpStatusCode.OK)
             {
-
+                Console.WriteLine($"Error accessing {cloud}values, this could be a breaking change");
+                Environment.Exit(32);
+            }
+            else {
+                Console.WriteLine($"Success accessing {cloud}values");
             }
 
         }
